59d62c9d629099cd05db3082b6680eb0ae811b10
Haidara <asumaila007@st.ug.edu.gh>
2022-08-04 12:40:47 +0000

    cleaned binaries for merge

--- flights/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from flights.models import *
-
-class FlightAdmin(admin.ModelAdmin):
-    list_display = ("id","origin","destination","duration")
-# Register your models here.
-admin.site.register(Flight, FlightAdmin)
-admin.site.register(Airport)
-admin.site.register(Passenger)
